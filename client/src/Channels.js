9bed7f37c20ebc4ef97763254bc8bdf96649cc7c
paulotijero <paulotijero@gmail.com>
2019-05-31 08:56:36 -0500

    Add channel user

--- client/src/Channels.js
+++ client/src/Channels.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { css } from "emotion";
 
+// import ChannelUser from "./"
+
 const className = css`
   grid-area: channels;
-  background-color: peru;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #2f3136;
 `;
 
 function Channels() {
   return (
     <section className={className}>
       <div>Channels</div>
+      <Channels />
     </section>
   );
 }
