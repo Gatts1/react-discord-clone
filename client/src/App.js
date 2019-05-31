4d1231d8478f2f0dbad263a6bbc89235f896ee46
condef5 <davis.con.fab@gmail.com>
2019-05-31 17:07:53 -0500

    Add websocket

--- client/src/App.js
+++ client/src/App.js
@@ -21,14 +21,55 @@ const Container = styled.div`
     "channels new-message";
 `;
 
-function App() {
+function App({ url = "ws://localhost:4000" }) {
+  const ws = React.useRef(null);
+  const [messages, setMessages] = React.useState([]);
+  const [connected, setConnected] = React.useState(false);
+
+  React.useEffect(() => {
+    const server = new WebSocket(url);
+    ws.current = server;
+    return () => {
+      ws.current = null;
+      server.close();
+    };
+  }, [url]);
+
+  React.useEffect(() => {
+    if (ws.current) {
+      ws.current.onopen = () => {
+        console.log("open");
+        setConnected(true);
+      };
+      ws.current.onclose = () => {
+        console.log("close");
+        setConnected(false);
+      };
+      ws.current.onmessage = ({ data }) => {
+        setMessages((state) => {
+          return [...state, JSON.parse(data)];
+        });
+      };
+    }
+  }, [ws.current]);
+
+  function sendData(content, type = "message") {
+    ws.current.send(
+      JSON.stringify({
+        id: new Date().getTime(),
+        content: content,
+        type
+      })
+    );
+  }
+
   return (
     <Container>
       <AppName />
       <Header />
       <Channels />
-      <ListMessages />
-      <NewMessage />
+      <ListMessages messages={messages} />
+      <NewMessage sendData={sendData} />
     </Container>
   );
 }
