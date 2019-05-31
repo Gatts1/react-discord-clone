2553649b5dcdf892af290c90ca3b1a5024b75b99
Marieth <mperezzevallos@gmail.com>
2019-05-31 12:28:30 -0500

    Added components for header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { css } from "emotion";
 
-const className = css`
+import HeaderName from "./HeaderName";
+import HeaderOptions from "./HeaderOptions";
+
+const container = css`
   grid-area: header;
-  background-color: purple;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+  background-color: #36393f;
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
 `;
 
 function Header() {
   return (
-    <section className={className}>
-      <div>Header</div>
+    <section className={container}>
+      <HeaderName />
+      <HeaderOptions />
     </section>
   );
 }
---
+++ client/src/HeaderName.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { css } from "emotion";
+
+const container = css`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  align-items: center;
+`;
+
+const name = css`
+  font-size: 15px;
+  color: #f6f6f7;
+  padding: 0;
+`;
+
+const svg = css`
+  width: 24px;
+  height: 24px;
+  margin: 0 8px;
+  fill: #72767d;
+`;
+
+function HeaderName() {
+  return (
+    <div className={container}>
+      {/* <svg id="btnMenu" onclick="showLeft()"><use xlink:href="#menu"></svg> */}
+      <svg className={svg}>
+        <use xlinkHref="#hashtag" />
+      </svg>
+      <span className={name}>general</span>
+    </div>
+  );
+}
+
+export default HeaderName;
