25272eddbd1ed8df5bdf21d15d1040bedee36511
condef5 <davis.con.fab@gmail.com>
2019-05-31 15:58:43 -0500

    Add login to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./App";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import Login from './Login/index';
 
-const $root = document.getElementById("root");
-render(<App />, $root);
+function Index() {
+  const isLoggin = true;
+  if (isLoggin) {
+    return <App />;
+  }
+  return <Login />;
+}
+
+const $root = document.getElementById('root');
+render(<Index />, $root);
