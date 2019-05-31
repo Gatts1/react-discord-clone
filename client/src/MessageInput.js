61a1bf4123998d62a9aba7975423ec84d5d2d4b7
Cesar Cachay <cesarcachay1996@gmail.com>
2019-05-31 12:43:39 -0500

    Delete unnecesary lines

--- client/src/MessageInput.js
+++ client/src/MessageInput.js
@@ -12,11 +12,6 @@ const container = css`
   padding: 10px;
 `;
 
-const writeMessage = css`
-  display: flex;
-  width: 100%;
-`;
-
 const svgs = css`
   width: 21px;
   height: 21px;
@@ -29,20 +24,6 @@ const svgs = css`
   }
 `;
 
-const writeMessageInputText = css`
-  width: 100%;
-  padding-left: 5px;
-  border: none;
-  font-size: 14px;
-  color: #f6f6f7;
-  border-left: 1px solid #55585d;
-  background-color: rgb(72, 75, 81);
-`;
-
-const hiddenSubmit = css`
-  display: none;
-`;
-
 function MessageInput() {
   return (
     <div className={container}>
@@ -52,20 +33,6 @@ function MessageInput() {
       <svg id="btnPlusCircle" className={svgs}>
         <use xlinkHref="#plus-circle" />
       </svg>
-      {/* <div className={writeMessage}>
-        <form action="" id="sendMessageForm">
-          <label for="txtMessage" aria-label="Recieve message" />
-          <input
-            className={writeMessageInputText}
-            type="text"
-            id="txtMessage"
-            placeholder="Send message to #general"
-            autocomplete="off"
-          />
-          <br />
-          <input type="submit" value="Send" class={hiddenSubmit} />
-        </form>
-      </div> */}
       <WriteMessage />
       <svg id="btnGift" className={svgs}>
         <use xlinkHref="#gift" />
