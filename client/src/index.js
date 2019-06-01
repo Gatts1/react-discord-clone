7ca1f933e79998a6b21dc4542fd300970c6b06ef
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-05-31 23:25:10 -0500

    Feature/login logic (#30)
    * Add save user
    
    * Remove class from icons
    
    * Add structure fakeData
    
    * Add logic to login

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import Login from './Login/index';
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import App from "./App";
+import Login from "./Login/index";
+import { newAppObject } from "./utils/storage";
+import "./utils/fakeData"; // load fake data, remove when pass to production
 
 function Index() {
-  const isLoggin = true;
-  if (isLoggin) {
+  const [user, setUser] = useState(null);
+
+  // this run once when component is render first time
+  useEffect(() => {
+    const app = JSON.parse(localStorage.getItem("app")) || null;
+    if (app) {
+      setUser(app.currentUser);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(user);
+    newAppObject(user);
+  }, [user]);
+
+  if (user) {
     return <App />;
   }
-  return <Login />;
+  return <Login setUser={setUser} />;
 }
 
-const $root = document.getElementById('root');
+const $root = document.getElementById("root");
 render(<Index />, $root);
---
+++ client/src/utils/storage.js
@@ -0,0 +1,32 @@
+function newAppObject(newUsername) {
+  const newId = Date.now();
+  const objApp = {
+    currentUser: {
+      id: newId,
+      username: newUsername,
+      creationDate: new Date().toLocaleString(),
+      state: "active"
+    },
+    users: [
+      {
+        id: newId,
+        username: newUsername,
+        isActive: true
+      }
+    ],
+    channels: [
+      {
+        id: 1000000000000,
+        creationDate: null,
+        name: "general",
+        author: null,
+        joined: false,
+        visibility: true,
+        messages: []
+      }
+    ]
+  };
+  localStorage.setItem("app", JSON.stringify(objApp));
+}
+
+export { newAppObject };
