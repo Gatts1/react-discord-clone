e63bf028b5404ef0b21754d76361d4c740a923c5
Cesar Cachay <cesarcachay1996@gmail.com>
2019-06-01 22:53:08 -0500

    fix typo

--- src/SendNotification.js
+++ src/SendNotification.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function SendNotification(currentUser) {
   if (
-    currentUser.user !== currentUser.name
+    currentUser.user !== currentUser.username
     // currentUser.channel != channelId
   ) {
     const notification = new Notification(
       // `New message in channel "${currentUser.channel}"`,
       `New message in channel`,
       {
-        body: `${currentUser.user}: ${currentUser.content}`,
+        body: `${currentUser.username}: ${currentUser.content}`,
         icon: "./icons/logo.js"
       }
     );
